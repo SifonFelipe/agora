eaf85e6d867bdb55ea24a370b5bc73319c754ea5
SifonFelipe <supermegafelipe@gmail.com>
2026-07-31 20:01:17 -0300

    moved uv for backend to agora_backend and moved to postgresql

--- agora_backend/agora_backend/settings.py
+++ agora_backend/agora_backend/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'agora_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'agora',
+        'USER': 'agora',
+        'PASSWORD': 'agoradb123',
+        'HOST': 'localhost',  # for now, later a server postgres
+        'PORT': '5432'
     }
 }
 
